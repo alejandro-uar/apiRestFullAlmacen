cc7359462bd32bbe5e4c6bfe18e57096c21fed64
Alejandro Luna <alejandro2002.uar@gmail.com>
2024-08-13 17:57:50 -0300

    feat: Add functions(index, store, show) - Model Producto

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,41 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
 use App\Models\Producto;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ProductoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        try{
+            return ApiResponse::success('Productos obtenidos', 200, Producto::all());
+        }catch(Exception $e){
+            return ApiResponse::error('Error al obtener los productos '.$e->getMessage(), 500);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        try{
+            $request->validate([
+                'nombre'=>'required|unique:productos',
+                'precio'=>'required|numeric|between:0,999999.99', 
+                'cantidad_disponible'=>'required|integer',
+                'categoria_id'=>'required|exists:categorias,id',
+                'marca_id'=>'required|exists:marcas,id',
+            ]);
+            $producto = Producto::create($request->all());
+            return ApiResponse::success('Producto creado exitosamente',201,$producto);
+        }catch(ValidationException $e){
+            $errors = $e->validator->errors()->toArray();
+            return ApiResponse::error('Errores de validacion '.$e->getMessage(), 422, $errors);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Producto $producto)
+    public function show($id)
     {
-        //
+        try{
+            $producto = Producto::findOrFail($id);
+            return ApiResponse::success('Producto obtenido exitosamente!', 200, $producto);
+        }catch(ModelNotFoundException $e){
+            return ApiResponse::error('Producto no encontrado',404);
+        }
     }
 
     /**
