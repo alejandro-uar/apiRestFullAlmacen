b693ee34c2f3e1cfc5deeddc002f4b61327ac38b
Alejandro Luna <alejandro2002.uar@gmail.com>
2024-08-13 16:54:31 -0300

    feat: Add function update - Model Marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -20,20 +20,9 @@ class MarcaController extends Controller
         }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
